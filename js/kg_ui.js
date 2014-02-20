f8196b9240c10ac0295a5bbc00ddd1b6fd6e7171
nchime <nchime@gmail.com>
2014-02-20 19:27:57 +0900

    ..
    ..

--- js/kg_ui.js
+++ js/kg_ui.js
@@ -1,6 +1,7 @@
 (function () {
     var Class = {
         winHeight: 0
+        , popZIndex: 5000
 		, winWidth: 0
 		, isMobile: navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i) ? true : false
 		, isWide: false
@@ -121,6 +122,51 @@
 				})
 
         }
+        /* 레이어 팝업 버튼 세팅 */, initLayerPopupOnce: function initLayerPopupOnce() {
+            $(document)
+				.on('click', 'a.jsbtnLyp', function (e) {
+				    var popID = $(this).attr('rel');
+				    Class.layerPopupOpen(popID);
+				    
+				    e.preventDefault();
+				    e.stopPropagation();
+				})
+				.on('click', '.lyPopWrap .jsPopClose', function (e) {
+				    $(this).parents('.lyPopWrap').trigger('closePopup');
+				    e.preventDefault();
+				})
+				.on('closePopup', '.lyPopWrap', function (e) {
+				    $(this).hide();
+				    $('#popFade').hide();
+				    if ($('.popWrap').length < 1)
+				        $('html').removeClass('hidden');
+
+				    e.stopPropagation();
+				});
+        }
+
+        /* Layer Popup 열기 */, layerPopupOpen: function layerPopOpen(id) {
+            var $popObj = $('#' + id);
+            $('body').append($popObj);
+
+            $popObj.show().css({ 'width': 300 }); //Number(width)
+
+            var popMargTop = ($popObj.height()) / 2;
+            var popMargLeft = ($popObj.width()) / 2;
+
+            $popObj.css({
+                'margin-top': -popMargTop,
+                'margin-left': -popMargLeft
+            });
+
+            var $wrap = $('body');
+
+            if ($wrap.find('#popFade').length < 1) {
+                $wrap.append('<div id="popFade"></div>')
+            }
+            $('#popFade').show();
+            $('html').addClass('hidden');
+        }
         /* 레이아웃 세팅 */, initLayoutOnce: function initLayoutOnce() {
             $(document).on('setLayout', function () {
                 var $wrap = $('#wrap');
@@ -135,9 +181,9 @@
                 
                 $header.css({ minWidth: Class.winWidth });
                 $content.css({ minHeight: $wrapH - $headerH, height: $contWrapH });
-
+                //alert($snbH)
                 if (!Class.isWide) {
-                    $snb.css({ height: $snbH, minHeight: $wrapH - $headerH });
+                    $snb.css({ height: '100%' , minHeight: $wrapH - $headerH });
                     $content.css({ minWidth: Class.winWidth, width: Class.winWidth});
                 } else {
                     if (!Class.isMobile) {
@@ -193,6 +239,22 @@
                 }
             }
         }
+		/* tab 세팅 */	,initTabOnce: function initTabOnce() {
+			$(document)
+				.on('click', 'ul.tabs>li', function (e) {
+					var $current = $(this);
+					var index = $current.index();
+					var $panels = $current.parent().siblings('.panels');
+					var cls = $current.attr('class').match(/tc[0-9]*/)[0];
+
+					$current.addClass(cls + '-selected').siblings().removeClass(cls + '-selected');
+					var $target = $panels.find('>.' + cls + '-panel:eq(' + index + ')');
+					$target.addClass(cls + '-selected').siblings().removeClass(cls + '-selected');
+
+					e.stopPropagation();
+					e.preventDefault();
+				});
+		}
     };
 
     if (typeof this['kgUI'] !== 'undefined') {
@@ -206,24 +268,12 @@
 //select Control
 $.fn.jsSelectCtrl = function () {
     var $wrapper = $(this);
-    var $mBoardli = $('.mBoardWrap > ul li');
-    var $reservationli = $('.reservationList li');
-	var $peopleView = $('.peopleView li');
 
-    $wrapper.find('.mBoardWrap li').click(function () {
-        $($mBoardli).removeClass('on');
-        $(this).addClass('on');
+	$wrapper.find('.jsTab li').click(function () {
+	    $('.jsTab li').removeClass('on');
+	    $(this).addClass('on');
+	    $(document).trigger('setLayout');
     })
-
-    $wrapper.find('.reservationList li').click(function () {
-        $($reservationli).removeClass('on');
-        $(this).addClass('on');
-    })
-
-	$wrapper.find('.peopleView li').click(function () {
-		$($peopleView).removeClass('on');
-		$(this).addClass('on');
-	})
 
 };
 
